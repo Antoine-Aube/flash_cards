aa635df7a708c9559c33330cd43915426f5699da
Antoine Aube <aaube3@gmail.com>
2024-02-05 21:51:24 -0700

    test: initialize and first instance method tests for round class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,6 +1,7 @@
 require './lib/turn'
 require './lib/card'
 require './lib/deck'
+require './lib/round'
 
 RSpec.describe Deck do 
 
@@ -10,18 +11,29 @@ RSpec.describe Deck do
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     @cards = [@card_1, @card_2, @card_3]
     @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
   end
 
-  describe "instance variables" do 
-    
-    it "#count" do
-      expect(@deck.count).to eq(3)
+  describe "intialiuze" do 
+    it "exists and has a deck, and empty turns" do 
+      expect(@round).to be_a(Round)
+
+      expect(@round.deck).to eq(@deck)
+    end
+  end
+
+  describe "instance methods" do 
+    it "#current_card" do
+      expect(@round.current_card).to eq(@card_1)
     end
 
-    it "#cards_in_category" do
-      expect(@deck.cards_in_category(:STEM)).to eq([@card_2, @card_3])
-      expect(@deck.cards_in_category(:Geography)).to eq([@card_1])
-      expect(@deck.cards_in_category("Pop Culture")).to eq([])
+    it "#take_turn" do 
+      new_turn = @round.take_turn("Juneau")
+      # require 'pry';binding.pry
+      expect(new_turn).to be_a(Turn)
+      expect(new_turn.correct?).to eq(true)
     end
+
+    it "expect"
   end
 end
